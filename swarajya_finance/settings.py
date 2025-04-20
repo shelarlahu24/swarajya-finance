989728ef9bbcbf7dd6982a0894ac611bff4d5cd9
Lahu Shelar <shelar.lahu24@gmail.com>
2025-04-20 20:33:46 +0530

    update .env.file

--- swarajya_finance/settings.py
+++ swarajya_finance/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['swarajya-finance.up.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['swarajya-finance.up.railway.app']
 
 
 # Application definition
